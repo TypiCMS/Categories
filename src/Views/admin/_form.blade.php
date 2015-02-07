ab83269cec1c37c2e4d0e01d6bf587fa35074aa6
Samuel De Backer <sdebacker@gmail.com>
2015-02-07 10:20:16 +0100

    form

--- src/Views/admin/_form.blade.php
+++ src/Views/admin/_form.blade.php
@@ -20,21 +20,7 @@
     @foreach ($locales as $lang)
 
     <div class="tab-pane fade @if ($locale == $lang)in active @endif" id="{{ $lang }}">
-        <div class="row">
-            <div class="col-md-6 form-group">
-                {!! BootForm::text(trans('labels.title'), $lang.'[title]') !!}
-            </div>
-            <div class="col-md-6 form-group @if($errors->has($lang.'.slug'))has-error @endif">
-                {{ Form::label($lang.'[slug]', trans('validation.attributes.slug')) }}
-                <div class="input-group">
-                    {{ Form::text($lang.'[slug]', $model->translate($lang)->slug, array('class' => 'form-control')) }}
-                    <span class="input-group-btn">
-                        <button class="btn btn-default btn-slug @if($errors->has($lang.'.slug'))btn-danger @endif" type="button">@lang('validation.attributes.generate')</button>
-                    </span>
-                </div>
-                {!! $errors->first($lang.'.slug', '<p class="help-block">:message</p>') !!}
-            </div>
-        </div>
+        @include('core::admin._title-and-slug')
         {!! BootForm::checkbox(trans('labels.online'), $lang.'[status]') !!}
     </div>
 
