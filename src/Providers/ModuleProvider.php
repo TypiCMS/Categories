b7de154437a56d1ebc06fdbe96f1652c261247ad
Samuel De Backer <sdebacker@gmail.com>
2015-04-27 17:54:03 +0200

    not linkable to page

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -26,7 +26,7 @@ class ModuleProvider extends ServiceProvider
         );
 
         $modules = $this->app['config']['typicms']['modules'];
-        $this->app['config']->set('typicms.modules', array_merge(['categories' => ['linkable_to_page']], $modules));
+        $this->app['config']->set('typicms.modules', array_merge(['categories' => []], $modules));
 
         $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'categories');
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'categories');
