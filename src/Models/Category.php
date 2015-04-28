bac88ce3d6f5b0cc8c14cceca4723515d5aefebe
Samuel De Backer <sdebacker@gmail.com>
2015-04-28 10:33:16 +0200

    publicUri method

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -49,33 +49,6 @@ class Category extends Base
     );
 
     /**
-     * Get public uri
-     *
-     * @return string|null string or void
-     */
-    public function getPublicUri($preview = false, $index = false, $lang = null)
-    {
-        if (! $this->hasTranslation($lang)) {
-            return null;
-        }
-        $lang = $lang ? : App::getlocale() ;
-        $parameters = [$this->translate($lang)->slug];
-        if (! $preview && ! $this->translate($lang)->status) {
-            $parameters = [null];
-        }
-        $route = array();
-        $route['lang'] = $lang;
-        $route['table'] = 'projects';
-        $route['category'] = 'categories';
-
-        $routeName = implode('.', $route);
-        if (Route::has($routeName)) {
-            return route($routeName, $parameters);
-        }
-        return null;
-    }
-
-    /**
      * Relations
      */
     public function projects()
