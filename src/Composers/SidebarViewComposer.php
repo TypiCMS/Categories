27997460a74e1825c7f10a7a89525c0c0980eeed
Samuel De Backer <sdebacker@gmail.com>
2015-05-24 22:43:54 +0200

    …

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -17,7 +17,7 @@ class SidebarViewComposer extends BaseSidebarViewComposer
                 $item->route('admin.categories.index');
                 $item->append('admin.categories.create');
                 $item->authorize(
-                    $this->user->hasAccess('categories.index')
+                    $this->auth->hasAccess('categories.index')
                 );
             });
         });
