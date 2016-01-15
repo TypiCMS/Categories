708efdb6326fe3063dbd6a8900ab8ee2de9075c3
Samuel De Backer <sdebacker@gmail.com>
2016-01-15 14:06:25 +0100

    new lang switcher

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -8,18 +8,6 @@
 
 @include('core::admin._image-fieldset', ['field' => 'image'])
 
-@include('core::admin._tabs-lang')
-
-<div class="tab-content">
-
-    @foreach ($locales as $lang)
-
-    <div class="tab-pane fade @if ($locale == $lang)in active @endif" id="{{ $lang }}">
-        @include('core::form._title-and-slug')
-        <input type="hidden" name="{{ $lang }}[status]" value="0">
-        {!! BootForm::checkbox(trans('validation.attributes.online'), $lang.'[status]') !!}
-    </div>
-
-    @endforeach
-
-</div>
+@include('core::form._title-and-slug')
+{!! TranslatableBootForm::hidden('status')->value(0) !!}
+{!! TranslatableBootForm::checkbox(trans('validation.attributes.online'), 'status') !!}
