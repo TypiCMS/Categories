b906ea42a2f7eb623c85ba6d1b8ad1c6326749fe
Samuel De Backer <sdebacker@gmail.com>
2016-09-30 11:00:27 +0200

    repo update

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -77,7 +77,7 @@ class AdminController extends BaseAdminController
      */
     public function update(Category $category, FormRequest $request)
     {
-        $this->repository->update($request->all());
+        $this->repository->update(request('id'), $request->all());
 
         return $this->redirect($request, $category);
     }
